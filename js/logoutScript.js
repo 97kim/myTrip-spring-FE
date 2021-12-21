aa9a0215d11cc4876a3fde15456c23fc62cdcfe3
hyojin-dev <wkdgywls30@gmail.com>
2021-12-21 16:22:57 +0900

    update

--- js/logoutScript.js
+++ js/logoutScript.js
@@ -1,3 +1,10 @@
+function logoutScript() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    alert('로그아웃을 완료했습니다.');
+    window.location.href = '../templates/index.html';
+}
+
 function deleteUser() {
     if (localStorage.getItem('token')) {
         $.ajax({
